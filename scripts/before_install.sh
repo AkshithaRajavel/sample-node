43c0b3617ccddf618c0f9919265a0a3270db4612
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 01:15:37 +0530

    version1.0

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
-sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo dpkg -i --force-overwrite /var/cache/apt/archives/nodejs_18.16.1-deb-1nodesource1_amd64.deb
 sudo apt-get update
 sudo apt-get install nodejs -y
