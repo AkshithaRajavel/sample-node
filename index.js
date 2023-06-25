7834343ad1b03e7df4379bfba4a254a69ce02c6f
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 01:48:50 +0530

    version1.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.status(200).send("status ok");
+    res.status(200).send("Helllllll Yeaaaahhhhhhh!!!! :)");
 })
 app.listen(4000,console.log('server ready...'))
