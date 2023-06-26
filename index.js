a07e5b8f7970472e50bf4c968ca25c18a078fbde
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 12:42:08 +0530

    pipeline-test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.status(200).send("First devops pipeline :)");
+    res.status(200).send(
+    `<h1>First devops pipeline</h1>
+    <li>github</li>
+    <li>AWS code pipeline</li>
+    <li>AWS code deploy</li>
+    <li>docker</li>`);
 })
 app.listen(4000,console.log('server ready...'))
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/nodejs
-sudo docker stop sample_app
+sudo docker stop -t0 sample_app
 sudo docker rmi sample_node
 sudo docker build . -t sample_node
