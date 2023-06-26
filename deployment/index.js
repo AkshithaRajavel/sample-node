ff5ef06228b0d1269a701f02a889c36892ee00ce
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 15:05:53 +0530

    react-build

--- deployment/index.js
+++ deployment/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
-})
+app.use(express.static(__dirname+'/public'))
 app.listen(4000,console.log('server ready...'))
