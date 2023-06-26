ecda60adc919bc1daee87fa640b4fd3931bb76cd
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 12:32:06 +0530

    pipeline-test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.status(200).send("First CI/CD pipeline :)");
+    res.status(200).send("First devops pipeline :)");
 })
 app.listen(4000,console.log('server ready...'))
