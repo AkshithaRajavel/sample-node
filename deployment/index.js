6ad509efa5d72c27a67a28751bfe6dca2d6b6010
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 17:37:11 +0530

    react-build

--- deployment/index.js
+++ deployment/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname+'/build'))
-app.listen(4000,console.log('server ready...'))
+const path = require('path');
+app.use(express.static(path.join(__dirname,'/build')));
+app.listen(4000,console.log(path.join(__dirname,'/build')));
