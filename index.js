99a6c09b548c096c74a698b3e42a4f371009cca7
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 13:53:33 +0530

    pipeline-test

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.status(200).send(
-    `<h1>First devops pipeline</h1>
-    <li>github</li>
-    <li>AWS code pipeline</li>
-    <li>AWS code deploy</li>
-    <li>docker</li>`);
+    res.sendFile(__dirname+'/index.html');
 })
 app.listen(4000,console.log('server ready...'))
