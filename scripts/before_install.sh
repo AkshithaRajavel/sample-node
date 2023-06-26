9d5433ee4e50e685413a8d92baf8cad6731f9a2d
AkshithaRajavel <2407akshi@gmail.com>
2023-06-26 11:04:53 +0530

    pipeline-test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
-#sudo rm -rf /home/ubuntu/nodejs
+sudo rm -rf /home/ubuntu/nodejs
 sudo dpkg -i --force-overwrite /var/cache/apt/archives/nodejs_18.16.1-deb-1nodesource1_amd64.deb
 sudo apt-get update
 sudo apt-get install nodejs -y
